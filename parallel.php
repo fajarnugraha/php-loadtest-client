8b17d97d36b8a7cf6593b9678ada6619565dba5a
FAN <git@fajar.net>
2020-12-31 13:43:45 +0700

    optimize some variable passing

--- parallel.php
+++ parallel.php
@@ -43,28 +43,29 @@ function get_url($cin, $cout, $params) {
 		$response = $c->request($target, ['timeout' => 
 			['connect' => $params['timeout']['connect'], 'request' => $params['timeout']['request']]
 		]);
+		unset($c);
 
 		$cout->push(["tid"=>$tid, "cid"=> $cid, "url_id"=>$in["id"], "url"=>$url,
 			"result"=>[
 				"error" => $response['error'],
-				"info" => &$response['info'],
+				"info" => $response['info'],
 				"headers" => $response['headers'],
 				"body_size" => $response['length'],
 				"body_sample" => $response['sample'],
-				#"body" => &$response['body'],
+				#"body" => $response['body'],
 			]
 		]);
+		unset($response);
 	}
 };
 
 $results = [];
-echo "Starting memory usage: ".memory_get_usage_mb()."\n";
 
 for ($i = 0; $i < $params["max"]["url"]; $i++) {
 	$urls[$i] = $base_url."&i=$i";
 }
 
-echo "Getting ".number_format(count($urls))." urls in ".number_format($params["max"]["thread"])." parallel threads";
+echo "Getting ".number_format(count($urls))." url(s) in ".number_format($params["max"]["thread"])." thread(s)";
 if ($params["max"]["url"] >= $params["max"]["column"]) echo "\n";
 $s = microtime(true);
 
@@ -74,7 +75,7 @@ $outs=[];
 $results=[];
 
 echo "\n";
-Co\run(function() use (&$urls, &$outs, $params) {
+Co\run(function() use ($urls, &$outs, $params) {
 	$cin = new Swoole\Coroutine\Channel(min($params["max"]["channel"], $params["max"]["url"]));
 	$cout = new Swoole\Coroutine\Channel(min($params["max"]["channel"], $params["max"]["url"]));
 
@@ -102,8 +103,7 @@ Co\run(function() use (&$urls, &$outs, $params) {
 });
 echo "\n";
 
-echo "took ".(microtime(true)-$s)."s\n";
-echo "memory usage: ".memory_get_usage_mb()."\n";
+echo "took ".number_format((microtime(true)-$s),3)."s, ".memory_get_usage_mb()." mem\n";
 echo "\n";
 
 $urls_max_index=count($urls)-1;
